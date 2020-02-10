846625b50d95d49c3cecc4614a84ad39a36a754f
HarrietApel <harryapel1@gmail.com>
2020-02-10 15:24:06 +0000

    refactor2: magic numbers for constants

--- clustering.py
+++ clustering.py
@@ -12,30 +12,31 @@ cluster_centre = [points[randrange(len(points))], points[randrange(len(points))]
 
 cluster_allocation = [None]*len(points)
 iteration = 0
-while iteration<10:
+iters = 10
+while iteration<iters:
     for i in range(len(points)):
         p = points[i]
-        distance = [None] * 3
-        # Assign each data point to a cluster.
+        distance = [None] * n_clusters
+        # Assign each data point to a cluster, specific to 3 clusters currently
         distance[0] = sqrt((p[0]-cluster_centre[0][0])**2 + (p[1]-cluster_centre[0][1])**2)
         distance[1] = sqrt((p[0]-cluster_centre[1][0])**2 + (p[1]-cluster_centre[1][1])**2)
         distance[2] = sqrt((p[0]-cluster_centre[2][0])**2 + (p[1]-cluster_centre[2][1])**2)
         cluster_allocation[i] = distance.index(min(distance))
     # Update the centre of each cluster by setting it to the average of all points assigned to the cluster
-    for i in range(3):
+    for i in range(n_clusters):
         cluster_points = [point for ind, point in enumerate(points) if cluster_allocation[ind] == i]
         new_mean = (sum([point[0] for point in cluster_points]) / len(cluster_points), sum([point[1] for point in cluster_points]) / len(cluster_points))
         cluster_centre[i] = new_mean
     iteration = iteration+1
 
-for i in range(3):
+for i in range(n_clusters):
     cluster_points = [point for ind, point in enumerate(points) if cluster_allocation[ind] == i]
     print("Cluster " + str(i) + " is centred at " + str(cluster_centre[i]) + " and has " + str(len(cluster_points)) + " points.")
 
 
 # Visualising the output of the algorithm
 from matplotlib import pyplot as plt
-for i in range(3):
+for i in range(n_clusters):
     cluster_points = [point for ind, point in enumerate(points) if cluster_allocation[ind] == i]
     plt.scatter([point[0] for point in cluster_points], [point[1] for point in cluster_points])
 plt.show()
